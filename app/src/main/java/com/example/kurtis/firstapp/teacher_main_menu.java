2daec2ddfec583dc4d156079a3da9f5c4b30e8f3
Kurtis <kurtis.aberhart@gmail.com>
2017-09-01 02:23:00 +1200

    Basic layout for teacher menu, I wasn't sure how to test if a teacher or student logs in..

---
+++ app/src/main/java/com/example/kurtis/firstapp/teacher_main_menu.java
@@ -0,0 +1,13 @@
+package com.example.kurtis.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class teacher_main_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout.activity_teacher_main_menu);
+    }
+}
